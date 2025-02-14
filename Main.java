5a3c9e5ce887b6abf114960009d8ef146cd6c37d
David Ayoub <6321852@edu.vaniercollege.qc.ca>
2025-02-14 14:41:06 -0500

    Task1,2,3

--- Main.java
+++ Main.java
@@ -15,11 +15,11 @@ public class Main {
 
     public static double avg(double[] nums) {
         if(nums.length == 0) return 0;
-         return sum(nums) / nums.length;
+        return sum(nums) / nums.length;
     }
 
     public static double min(double[] nums) {
-        if(nums.length == 0) return -1;
+        if (nums.length == 0) return -1;
         double min = nums[0];
         for (double num : nums) {
             if(num < min) {
@@ -29,4 +29,29 @@ public class Main {
         return min;
     }
 
+    public static double calcFinalScore(double[] scores) {
+        double min = scores[0];
+        double max = scores[0];
+        double sum = 0;
+        for (double score : scores) {
+            sum += score;
+            if (score < min) {
+                min = score;
+            } else if (score > max) {
+                max = score;
+            }
+        }
+        sum -= max;
+        sum -= min;
+        double scoresLeft = scores.length - 2;
+        return sum / scoresLeft;
+    }
+
+    public static double[] minOfRow(double[][] numss) {
+
+    }
+
+    public static double[] minOfCol(double[][] numss) {
+
+    }
 }
